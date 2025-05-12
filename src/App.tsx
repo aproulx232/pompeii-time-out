c5c058c015dd55912b91ad96befd0711038877e2
Allen Proulx <allen.proulx232@gmail.com>
2025-05-11 22:30:27 -0400

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import Game from './components/game/Game';
 function App() {
   return (
     <div className="App">
+      <header className="game-header">
+        <h1>Pompeii Time Out</h1>
+        <p className="game-tagline">Connect through time. Save ancient Pompeii.</p>
+      </header>
       <Game />
     </div>
   );
